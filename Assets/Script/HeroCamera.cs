fd316bc1604c9c7e866465d4c3720739bd96495d
植发伟 <whiteClouds2016@163.com>
2017-09-16 00:27:49 +0800

    修改ignore规则

---
+++ Assets/Script/HeroCamera.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+//添加脚本到component菜单
+[AddComponentMenu ("CameraControl/Follow")]
+public class HeroCamera : MonoBehaviour {
+
+	//摄像机固定在猪脚上方10米高度
+	public float camera_height=30.0f;
+	//摄像机离猪脚大概10米的水平距离
+	public float camera_distance=30.0f;
+
+	//摄像机和猪脚的transform属性
+	private Transform player;
+	private Transform camera;
+	// Use this for initialization
+	void Start () {
+		//初始化
+		player=GameObject.FindGameObjectWithTag("Player").transform;
+		camera = Camera.main.transform;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+
+		//看向猪脚
+		//camera.LookAt (player);
+
+
+		//与猪脚的正前方为正前方(只取Y轴的旋转度)
+		camera.eulerAngles =new Vector3(player.eulerAngles.x + 45,
+			player.eulerAngles.y - 45,
+			player.eulerAngles.z);
+		//获取当前的镜头的Y轴旋转度
+		float angle = camera.eulerAngles.y;
+
+		//计算x轴的距离差:
+		float deltaX = camera_distance * Mathf.Sin(angle * Mathf.PI /180 );
+		float deltaZ = camera_distance * Mathf.Cos (angle * Mathf.PI / 180);
+
+
+
+		//每一帧都改变摄像机的高度
+		camera.position = new Vector3 (player.position.x-deltaX,
+			player.position.y+ camera_height,
+			player.position.z-deltaZ);
+
+	}
+}
---
+++ Assets/Script/HeroController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroController : MonoBehaviour {
+
+	private Animator _animator;
+	private Rigidbody _rigidbody;
+	public float speed = 20f;
+
+	// Use this for initialization
+	void Start () {
+		_animator = this.GetComponent<Animator>();
+		_rigidbody = this.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.W))
+		{
+			_animator.SetBool("walk", true);
+		}
+		if(Input.GetKeyUp(KeyCode.W))
+		{
+			_animator.SetBool("walk", false);
+		}
+	}
+
+	void FixedUpdate()
+	{
+		MoveControlByTranslateGetAxis();
+	}
+
+	//Translate移动控制函数
+	void MoveControlByTranslateGetAxis()
+	{
+		float horizontal = Input.GetAxis("Horizontal"); //A D 左右
+		float vertical = Input.GetAxis("Vertical"); //W S 上 下
+
+//		transform.Translate(Vector3.forward * vertical * speed * Time.deltaTime);//W S 上 下
+//		transform.Translate(Vector3.right * horizontal * speed * Time.deltaTime);//A D 左右
+//
+
+		_rigidbody.MovePosition(this.transform.position + new Vector3(horizontal, 0, vertical) * speed * Time.deltaTime);
+		if(horizontal != 0f || vertical != 0f)
+		{
+			Rotating(horizontal, vertical);
+		}
+	}
+
+	void Rotating(float horizontal, float vertical)
+	{
+		// 创建角色目标方向的向量
+		Vector3 targetDirection = new Vector3(horizontal, 0f, vertical);
+		// 创建目标旋转值 并假设Y轴正方向为"上"方向
+		Quaternion targetRotation = Quaternion.LookRotation(targetDirection, Vector3.up); //函数参数解释: LookRotation(目标方向为"前方向", 定义声明"上方向")
+		// 创建新旋转值 并根据转向速度平滑转至目标旋转值
+		//函数参数解释: Lerp(角色刚体当前旋转值, 目标旋转值, 根据旋转速度平滑转向)
+		Quaternion newRotation = Quaternion.Lerp(_rigidbody.rotation, targetRotation, speed * Time.deltaTime);
+		// 更新刚体旋转值为 新旋转值
+		_rigidbody.MoveRotation(newRotation);
+
+	}
+}

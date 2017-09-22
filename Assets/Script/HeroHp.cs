e8060fc0099525fc617b1a28c7940b6f8ce1f2ad
植发伟 <whiteClouds2016@163.com>
2017-09-23 00:11:18 +0800

    添加学两条

---
+++ Assets/Script/HeroHp.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeroHp : MonoBehaviour {
+	public Slider hp;
+	public Text hpText;
+
+	private float width = 120;
+	private float height = 15;
+
+	private float valueMax = 100;
+	private float value = 100;
+
+	void Start () {
+		GetComponent<RectTransform>().sizeDelta = new Vector2(width, height);
+	}
+	
+
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.E))
+		{
+			value -= 10;
+		}
+	}
+
+	void FixedUpdate()
+	{
+		hpText.text = value + "/" + valueMax;
+		hp.value = value/valueMax;
+	}
+}

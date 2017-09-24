d5af554a733639c430bd15df2eb6ee263aac5ccf
植发伟 <whiteClouds2016@163.com>
2017-09-25 00:26:53 +0800

    修改启动页面

---
+++ Assets/Script/StartMenu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour {
+	public Button startButton;
+	public Button settingButton;
+	public Button quitButton;
+
+	// Use this for initialization
+	void Start () {
+		startButton.onClick.AddListener (delegate() {  
+			this.start();   
+		});
+
+		settingButton.onClick.AddListener (delegate() {  
+			this.setting();   
+		});
+
+		quitButton.onClick.AddListener (delegate() {  
+			this.quit();   
+		});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//开始游戏
+	public void start(){
+		Debug.Log ("启动游戏");
+		SceneManager.LoadScene ("scene_1");
+	}
+
+	//设置游戏
+	public void setting(){
+		Debug.Log ("设置游戏");
+	}
+
+	//退出游戏
+	public void quit(){
+		Debug.Log ("退出游戏");
+	}
+
+	void OnDestroy(){
+		startButton.onClick.RemoveAllListeners ();
+		settingButton.onClick.RemoveAllListeners ();
+		quitButton.onClick.RemoveAllListeners ();
+	}
+}

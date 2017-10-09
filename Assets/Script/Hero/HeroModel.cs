21ebefb1c3641276bf7f2ef13d016a22b8e20419
Neo <neo.zhi@aeasycredit.com>
2017-10-09 18:06:59 +0800

    修改英雄属性模板

--- Assets/Script/Hero/HeroModel.cs
+++ Assets/Script/Hero/HeroModel.cs
@@ -3,6 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HeroModel {
+	//力量
+    public float strength;
+    //敏捷
+    public float agility;
+    //智力
+    public float intellect;
+
     //血量
     public float hp;
     //能量
@@ -11,22 +18,50 @@ public class HeroModel {
     public float hpMax;
     //能量上限
     public float mpMax;
+	//基础血量恢复速度
+    public float basicHpRegeneration
+	{
+		get
+      	{
+			//每一点力量值增加0.06点生命恢复速率
+         	return strength * 0.06 + basicHpRegeneration; 
+      	}
+		set
+		{
+			basicHpRegeneration = value;
+		}
+    };
+	//额外血量恢复速度
+    public float additionalHpRegeneration;
+	//血量恢复速度
+	public float hpRegeneration
+	{
+		get
+      	{
+         	return basicHpRegeneration + additionalHpRegeneration; 
+      	}
+    };
+	//能量回复速度
+	public float mpRegeneration;
+
     //护甲
     public float armor;
     //速度
     public float speed;
-    //力量
-    public float strength;
-    //敏捷
-    public float agility;
-    //智力
-    public float intellect;
+    
+
     //基础攻击力
     public float basicAttack;
     //额外攻击力
     public float additionalAttack;
     //攻击力
-    public float attack;
+	public float attack{
+		get 
+      {
+         return basicAttack + additionalAttack; 
+      }
+    };
+
     //物理抗性
     public float physicalResistance;
     //魔法抗性

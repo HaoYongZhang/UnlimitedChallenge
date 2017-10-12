2bb6c23ca59e39e6d5375bd532bc5b604e849461
Neo <neo.zhi@aeasycredit.com>
2017-10-12 18:01:24 +0800

    #

---
+++ Assets/Script/Hero/Skill.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill
+{
+	//id
+	public int id;
+	//名字
+	public string name;
+	//类型
+	public SkillType type;
+	//图片
+	public string imageName;
+	//描述
+	public string description;
+
+	public Skill()
+	{
+
+	}
+}
+
+enum SkillType
+{
+	//攻击
+    Attack = 0,
+    //防御
+    Defense = 1,
+    //治疗
+    Treatment = 2,
+    //强化
+    Intensify = 3,
+    //特殊
+    Specialty = 4
+}

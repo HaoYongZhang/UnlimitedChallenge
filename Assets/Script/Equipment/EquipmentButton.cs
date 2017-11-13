88dbe8a32da3bfa5187f593d94e673d08ba93b24
Neo <neo.zhi@aeasycredit.com>
2017-11-13 18:01:21 +0800

    修UI界面

--- Assets/Script/Equipment/EquipmentButton.cs
+++ Assets/Script/Equipment/EquipmentButton.cs
@@ -10,6 +10,33 @@ public class EquipmentButton : MonoBehaviour {
     public EquipmentPart part;
     public Image icon;
 
+    Sprite defaultIcon;
+
+    public static EquipmentButton NewInstantiate()
+    {
+        EquipmentButton equipmentButton = Instantiate((GameObject)Resources.Load("UI/EquipmentButton")).GetComponent<EquipmentButton>();
+
+        return equipmentButton;
+    }
+
+    public static EquipmentButton NewInstantiate(Equipment _equipment)
+    {
+        EquipmentButton equipmentButton = NewInstantiate();
+        equipmentButton.equipment = _equipment;
+
+        return equipmentButton;
+    }
+
+    public static EquipmentButton NewInstantiate(EquipmentPart _part)
+    {
+        EquipmentButton equipmentButton = NewInstantiate();
+        equipmentButton.part = _part;
+        equipmentButton.defaultIcon = equipmentButton.getPartDefaultImage(_part);
+
+        return equipmentButton;
+    }
+
+
 	// Use this for initialization
 	void Start () {
 		
@@ -19,4 +46,39 @@ public class EquipmentButton : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    Sprite getPartDefaultImage(EquipmentPart _part){
+        string equipmentPath = "";
+
+        switch(_part)
+        {
+            case EquipmentPart.weapon:{
+                    equipmentPath = "Sword-Icon";
+                }
+                break;
+            case EquipmentPart.head:
+                {
+                    equipmentPath = "Head-Icon_2";
+                }
+                break;
+            case EquipmentPart.body:
+                {
+                    equipmentPath = "Torso-Icon";
+                }
+                break;
+            case EquipmentPart.legs:
+                {
+                    equipmentPath = "Legs-Icon";
+                }
+                break;
+            case EquipmentPart.treasure:
+                {
+                    equipmentPath = "Amulet-Icon-Small";
+                }
+                break;
+        }
+
+        Sprite imageSprite = Resources.Load("Equipment/" + equipmentPath, typeof(Sprite)) as Sprite;
+        return imageSprite;
+    }
 }
--- Assets/Script/UI/UIHeroItemView.cs
+++ Assets/Script/UI/UIHeroItemView.cs
@@ -2,14 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using EquipmentClass;
 
 public class UIHeroItemView : MonoBehaviour {
-    public Button leftWeapon;
-    public Button rightWeapon;
-    public Button head;
-    public Button body;
-    public Button legs;
-    public Button treasure;
+    public GameObject equipmentLeftView;
+    public GameObject equipmentRightView;
+
+    public EquipmentButton leftWeapon;
+    public EquipmentButton rightWeapon;
+    public EquipmentButton head;
+    public EquipmentButton body;
+    public EquipmentButton legs;
+    public EquipmentButton treasure;
 
     public Text leftWeaponText;
     public Text rightWeaponText;
@@ -18,10 +22,63 @@ public class UIHeroItemView : MonoBehaviour {
     public Text legsText;
     public Text treasureText;
 
+    List<string> equipmentNameList = new List<string>{"主武器", "副武器", "头部", "上半身", "下半身", "宝物"};
+    List<Text> equipmentTextList = new List<Text>();
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        leftWeapon = EquipmentButton.NewInstantiate(EquipmentPart.weapon);
+        rightWeapon = EquipmentButton.NewInstantiate(EquipmentPart.weapon);
+        head = EquipmentButton.NewInstantiate(EquipmentPart.head);
+        body = EquipmentButton.NewInstantiate(EquipmentPart.body);
+        legs = EquipmentButton.NewInstantiate(EquipmentPart.legs);
+        treasure = EquipmentButton.NewInstantiate(EquipmentPart.treasure);
+
+        leftWeaponText = new GameObject().AddComponent<Text>();
+        rightWeaponText = new GameObject().AddComponent<Text>();
+        headText = new GameObject().AddComponent<Text>();
+        bodyText = new GameObject().AddComponent<Text>();
+        legsText = new GameObject().AddComponent<Text>();
+        treasureText = new GameObject().AddComponent<Text>();
+
+        equipmentTextList.Add(leftWeaponText);
+        equipmentTextList.Add(rightWeaponText);
+        equipmentTextList.Add(headText);
+        equipmentTextList.Add(bodyText);
+        equipmentTextList.Add(legsText);
+        equipmentTextList.Add(treasureText);
+
+        for (int i = 0; i < equipmentTextList.Count; i++)
+        {
+            equipmentTextList[i].rectTransform.sizeDelta = new Vector2(120, 30);
+            equipmentTextList[i].text = equipmentNameList[i];
+            equipmentTextList[i].font = UIScene.Instance.skillInfo.GetComponentsInChildren<Text>()[1].font;
+            equipmentTextList[i].fontSize = 25;
+            equipmentTextList[i].alignment = TextAnchor.MiddleCenter;
+            equipmentTextList[i].color = new Color(255f, 255f, 255f);
+        }
+
+        //左边
+        leftWeapon.transform.SetParent(equipmentLeftView.transform, false);
+        leftWeaponText.transform.SetParent(equipmentLeftView.transform, false);
+
+        head.transform.SetParent(equipmentLeftView.transform, false);
+        headText.transform.SetParent(equipmentLeftView.transform, false);
+
+        treasure.transform.SetParent(equipmentLeftView.transform, false);
+        treasureText.transform.SetParent(equipmentLeftView.transform, false);
+
+        //右边
+        rightWeapon.transform.SetParent(equipmentRightView.transform, false);
+        rightWeaponText.transform.SetParent(equipmentRightView.transform, false);
+
+        body.transform.SetParent(equipmentRightView.transform, false);
+        bodyText.transform.SetParent(equipmentRightView.transform, false);
+
+        legs.transform.SetParent(equipmentRightView.transform, false);
+        legsText.transform.SetParent(equipmentRightView.transform, false);
+
+    }   
 	
 	// Update is called once per frame
 	void Update () {

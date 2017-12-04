8c33ea8f0497e016021de290514049bcad1ee2cd
Neo <neo.zhi@aeasycredit.com>
2017-12-04 17:57:24 +0800

    1.添加丧尸控制

---
+++ Assets/Script/NPC/Zombie/Zombie.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie : MonoBehaviour {
+    
+    public Rigidbody _rigid;
+    public Animator _animator;
+
+	// Use this for initialization
+	void Start () {
+        Invoke("test", 3f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void test()
+    {
+        Debug.Log("执行");
+        _animator.SetBool("fight", true);
+    }
+}
